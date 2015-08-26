e1863653667b0955899b2ac6b52ee3e7c7e0f062
Adrian Rakochi <adrianrakochi@gmail.com>
2015-08-25 23:54:08 -0400

    Changed commenting to match code change.

--- drive/ConfigItems.php
+++ drive/ConfigItems.php
@@ -2,7 +2,7 @@
 class ConfigItems
 {
    public $logFile;  //opened and ready-to-write-to log file
-   public $refreshToken; //name of refresh token file
+   public $refreshToken; //refresh token string
    public $uniqname;  //user's uniqname
 }
 ?>
