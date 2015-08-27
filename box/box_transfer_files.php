de1c6874484b98a6fdfda9f47aa1abe65bd4dc19
Adrian Rakochi <adrianrakochi@gmail.com>
2015-08-27 13:56:55 -0400

    Hassan added failed files list feature.

--- box/box_transfer_files.php
+++ box/box_transfer_files.php
@@ -1,10 +1,22 @@
 <?php
+	
+	/* Copyright 2015 Hassan Mahmood, The University of Michigan
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	    http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License. */
 
 	$numFolders = 0;
 	$numFiles = 0;
 	$totalFolders = count($folderList);
 	$totalFiles = count($fileList);
 	$choice = 'box';
+	$failedFiles = array();
 	
 
 	// Get the client ID, client secret, and redirect uri
@@ -142,15 +154,16 @@
 		}
 		else {
 			array_push($stringArray, "Folder '" . getFileName($key) . "' NOT successfully created: {$nextID}, ", "Error: {$nextID['code']}", "<br>");
+			array_push($failedFiles, $key);
 		}
 	}
 
 	file_put_contents($logName, $stringArray);
+	$numLines = 0;
 	
 	// Upload each of the user's files to Box
 	foreach ($fileList as $value) {
-		unset($stringArray);
-		$stringArray = array();
+		$numLines++;
 
 		// Make sure the file still exists in AFS
 		if (!file_exists($value->path)) {
@@ -196,9 +209,14 @@
 		}
 		else {
 			array_push($stringArray, "File '" . getFileName($value->path) . "' NOT successfully uploaded, ", "Error: {$nextID['code']}", "<br>");
+			array_push($failedFiles, $value->path);
 		}
 
-		file_put_contents($logName, $stringArray, FILE_APPEND);
+		if ($numLines > 100) {
+			file_put_contents($logName, $stringArray, FILE_APPEND);
+			$stringArray = array();
+			$numLines = 0;
+		}
 	}
 
 	array_push($stringArray, "<br>", "Successfully uploaded {$numFolders} folders and {$numFiles} files", "<br>");
@@ -209,6 +227,7 @@
 	// If the transfer completed successfully, delete the log file
 	if ($numFiles == $totalFiles and $numFolders == $totalFolders) {
 		unlink($logName);
+		unset($failedFiles);
 	}
 
 	// Send the notification email to the user
